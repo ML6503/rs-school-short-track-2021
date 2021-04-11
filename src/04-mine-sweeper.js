0aba98054a262d9825baaec4658570b11ba83753
ML6503 <ml6503@gmail.com>
2021-04-11 13:43:27 +0300

    feat: add solution to 4 tas

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,76 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const res = [...matrix];
+
+  const ifAllFalse = (matrix.map((arr) => arr.every((e) => e === false)));
+
+  if (ifAllFalse.filter((e) => e !== true).length === 0) {
+    matrix.map((arr, ind) => arr.map((e, i) => {
+      res[ind][i] = 0;
+      return res;
+    }));
+  }
+
+  matrix.map((arr, ind) => arr.map((e, i) => {
+    if (e === false) {
+      if ((matrix[ind][i - 1] === true && matrix[ind][i + 1] === true)
+          || (matrix[ind][i - 1] === true && matrix[ind + 1][i] === true)
+          || (matrix[ind][i + 1] === true && matrix[ind + 1][i] === true)
+          || (matrix[ind][i + 1] === true && matrix[ind - 1][i] === true)
+          || (matrix[ind][i - 1] === true && matrix[ind - 1][i] === true)
+          || (matrix[ind - 1] !== undefined && matrix[ind - 1][i + 1] === true
+            && matrix[ind - 1][i - 1] === true)
+          || (matrix[ind + 1] !== undefined && matrix[ind + 1][i + 1] === true
+            && matrix[ind + 1][i - 1] === true)) {
+        res[ind][i] = 2;
+      } if (((matrix[ind - 1] !== undefined && matrix[ind - 1][i + 1] === true)
+            || (matrix[ind - 1] !== undefined && matrix[ind - 1][i - 1] === true))
+            || ((matrix[ind + 1] !== undefined && matrix[ind + 1][i + 1] === true)
+            || (matrix[ind + 1] !== undefined && matrix[ind + 1][i - 1] === true))) {
+        res[ind][i] = 1;
+      }
+    }
+
+    return res;
+  }));
+  res.map((arr, ind) => arr.map((e, i) => {
+    if (typeof e === 'boolean') {
+      res[ind][i] = 1;
+    }
+    return res;
+  }));
+
+  return res;
 }
+
+// function minesweeper(matrix) {
+//   const newMatrix = [...matrix];
+//   const flat = [].concat.apply([], matrix);
+//   // const flat = [].concat.([...matrix]);
+//   console.log(matrix[1]);
+//   console.log(flat);
+//   flat.map((e, i) => {
+//     if (e = false && flat[i-1] === true || flat[i++] === true  || flat[i] ) {
+//       flat[i] = 1;
+
+//     }
+//   });
+//   return newMatrix;
+// }
+
+// const matrix = [
+//   [true, false, false],
+//   [false, true, false],
+//   [false, false, false],
+// ];
+
+// // const matrix = [
+// //   [false, false, false],
+// //   [false, false, false],
+// // ];
+// // eslint-disable-next-line no-console
+// console.log(minesweeper(matrix));
 
 module.exports = minesweeper;
--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,44 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+
+// if (num !== res[i] && highest < parseInt([num, res[i]].join(''), 10)) {
+//   highest = parseInt([num, res[i]].join(''), 10);
+//   // console.log('2 num', parseInt([num, res[i]].join(''), 10));
+// }
+
+// function deleteDigit(n) {
+//   let highest = 0;
+//   const res = n.toString(10).split('').map(Number);
+//   let compArr = [...res];
+
+//   const lowest = Math.min(...res);
+//   console.log('loww', lowest);
+
+//   for (let i = 0; i < res.length; i++) {
+//     // console.log('num', parseInt(res.splice(i, 1).join(''), 10));
+//     delete compArr[i];
+
+//     const compareNum = parseInt(compArr.join(''), 10);
+
+//     highest = compareNum;
+
+//     console.log("high", highest);
+
+//     if (highest < compareNum) {
+//       compArr = res;
+//       console.log(compArr);
+//     }
+
+//   }
+
+//   return highest;
+// }
+
+// console.log(deleteDigit(1001));
+
+function deleteDigit(n) {
+  return n;
 }
 
 module.exports = deleteDigit;
