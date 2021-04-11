dd7272a447051b4f50df14fc2201a096a8a2dc34
ML6503 <ml6503@gmail.com>
2021-04-12 00:24:48 +0300

    feat: add solution for task 10

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,39 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+// function findIndex(array, value) {
+//   // const res = array.indexOf(value);
+
+//   let res;
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] === value) {
+//       res = i;
+//     }
+//   }
+
+//   return res;
+// }
+
+function findIndex(array, value) {
+  // const res = array.indexOf(value);
+  let high = array.length - 1;
+  let i = 0;
+  let mid;
+
+  while (i <= high) {
+    mid = Math.floor((i + high) / 2);
+    if (array[mid] === value) {
+      return mid;
+    }
+    if (array[mid] < value) {
+      i = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return -1;
 }
+
+// console.log(findIndex([5, 6, 7], 5));
 
 module.exports = findIndex;
